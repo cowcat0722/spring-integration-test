1bb5f6568666387753f25e9a853877cae67aa123
Pakchan <1625787@donga.ac.kr>
2024-05-16 11:29:57 +0900

    Integration_Test : Join

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,13 +1,10 @@
 package shop.mtcoding.blog.user;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.blog._core.utils.ApiUtil;
 
@@ -21,7 +18,7 @@ public class UserController {
 
     // TODO: 회원정보 조회 API 필요 -> @GetMapping("/api/users/{id}")
     @GetMapping("/api/users/{id}")
-    public ResponseEntity<?> userinfo(@PathVariable Integer id){
+    public ResponseEntity<?> userinfo(@PathVariable Integer id) {
         UserResponse.DTO respDTO = userService.회원조회(id);
         return ResponseEntity.ok(new ApiUtil(respDTO));
     }
@@ -36,7 +33,7 @@ public class UserController {
     }
 
     @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody UserRequest.JoinDTO reqDTO) {
+    public ResponseEntity<?> join(@Valid @RequestBody UserRequest.JoinDTO reqDTO, Errors errors) {
         UserResponse.DTO respDTO = userService.회원가입(reqDTO);
         return ResponseEntity.ok(new ApiUtil(respDTO));
     }
@@ -44,7 +41,7 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserRequest.LoginDTO reqDTO) {
         String jwt = userService.로그인(reqDTO);
-        return ResponseEntity.ok().header("Authorization", "Bearer "+jwt).body(new ApiUtil(null));
+        return ResponseEntity.ok().header("Authorization", "Bearer " + jwt).body(new ApiUtil(null));
     }
 
     @GetMapping("/logout")
